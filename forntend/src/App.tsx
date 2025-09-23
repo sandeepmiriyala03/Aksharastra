8cf4cddd4593b2b6c144ca2187bbcea80763ec9d
Sandeep Miriyala <3719057+sandeepmiriyala03@users.noreply.github.com>
2025-09-23 23:21:05 +0530

    added code

--- forntend/src/App.tsx
+++ forntend/src/App.tsx
@@ -10,8 +10,9 @@ function App() {
 
   // Fetch backend welcome message on page load
   useEffect(() => {
-    fetch('https://aksharastra.onrender.com/')
+    fetch('https://aksharastra-oncm.onrender.com') 
       .then(async (res) => {
+     console.log(res);
         if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
         const data: ApiResponse = await res.json();
         setApiMessage(data.message);
@@ -35,7 +36,7 @@ function App() {
     try {
      // const localhost='http://localhost:8000';
   
-      const response = await fetch('https://aksharastra.onrender.com/generate-audio/', {
+      const response = await fetch('https://aksharastra-oncm.onrender.com/generate-audio/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: textInput, voice_rate: 150, voice_volume: 0.9 }),
--- forntend/vite.config.ts
+++ forntend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
-      // Additional workbox options here
+    
     })
   ]
 });
